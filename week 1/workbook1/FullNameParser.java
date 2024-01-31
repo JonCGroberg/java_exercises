3360a4ea44eacd0cf4da747939ffd962de76ad79
Jonathan Groberg <jongroberg@gmail.com>
2024-01-31 14:53:13 -0500

    FullNameParser

---
+++ week 1/workbook1/FullNameParser.java
@@ -0,0 +1,30 @@
+import com.sun.source.tree.ClassTree;
+
+import java.util.Scanner;
+
+public class FullNameParser {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your name(first last OR first middle last: ");
+
+        String[] userInput = scanner.nextLine().trim().split(" ");
+        String first, middle, last;
+
+        if (userInput.length == 3) {
+            first = userInput[0];
+            middle = userInput[1];
+            last = userInput[2];
+            System.out.printf("First name: %s\nMiddle name: %s\nLast name: %s", first
+                    , middle, last);
+        } else if (userInput.length == 2) {
+            first = userInput[0];
+            last = userInput[1];
+            System.out.printf("First name: %s\nLast name: %s", first
+                    , last);
+        } else {
+            System.out.println("Make sure you follow this format: first last OR first middle last");
+        }
+    }
+}
+
