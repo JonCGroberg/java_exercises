ec5b66260f6d436966092cba65f26aa93d02880a
Jonathan Groberg <jongroberg@gmail.com>
2024-02-09 12:23:53 -0500

    init commit

---
+++ week 2/JonsPizza/src/main/java/com/pluralsight/PizzaShopApplication.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class PizzaShopApplication {
+    public void start(){
+        System.out.println();
+        System.out.println("Welcome to Jon's Pizza");
+        System.out.println("----------------------");
+    }
+}
